0e430c9b70587b8e5048148f8f186576c95474d7
justbbarnett <justb@inkery.com>
2018-06-08 23:27:20 -0500

    server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var moment = require('moment');
 var mongoose = require('mongoose');
 
 var db = require('./models');
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 var app = express();
 
